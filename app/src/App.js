20179aa76e757d4248875166cedbb8d97ea2319a
S-Swaroop <itsmesharan2000@gmail.com>
2021-07-19 21:20:38 +0530

    added routes and basic navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Header from './components/Header/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Login from './components/Login/Login'
+import DashBoard from './components/DashBoard/DashBoard';
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      
+      <Router>
+
+        <Header/>
+
+      <Switch>
+        <Route path='/login' exact >
+          <Login />
+        </Route>
+        <Route path='/dashboard' exact >
+          <DashBoard />
+        </Route>
+      </Switch>
+
+      </Router>
     </div>
   );
 }
