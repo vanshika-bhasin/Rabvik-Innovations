fd9907f9429102a721d9693187b9cfe8cef7ab9a
Nossam Yaswanth <nossamyaswanth@gmail.com>
2021-07-20 14:23:14 +0530

    first update

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import SignUp from './components/pages/SignUp';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component= {Home} />
+          <Route path='/services' exact component= {Services} />
+          <Route path='/products' exact component= {Products} />
+          <Route path='/sign-up' exact component= {SignUp} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
