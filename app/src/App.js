25c842b4f39fec4bc5e3a0a07e58cfdf03aaec80
S-Swaroop <itsmesharan2000@gmail.com>
2021-07-22 01:22:56 +0530

    added sidebar for dashboard

--- app/src/App.js
+++ app/src/App.js
@@ -7,6 +7,7 @@ import SignUp from './components/pages/SignUp';
 import Login from './components/pages/Login';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
+import DashBoard from './components/pages/DashBoard/DashBoard';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path='/products' exact component= {Products} />
           <Route path='/sign-up' exact component= {SignUp} />
           <Route path='/login' exact component= {Login} />
+          <Route path='/dashboard' eaxct component={DashBoard} />
         </Switch>
       </Router>
     </div>
